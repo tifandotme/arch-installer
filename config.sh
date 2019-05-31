a781f34ee0b4a332cce2ae8e85d5f0320933c6fb
Tifan Dwi Avianto <ifan.anvity@gmail.com>
2019-05-31 08:37:37 +0700

    Added a few commands to install GRUB and create a new user

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # install.sh will call this seperate script to further configure the system
 
-# adjusting timezone
+# adjust timezone
 ln -sf /usr/share/zoneinfo/"$1" /etc/localtime
 hwclock --systohc
 
-# configuring locales
-sed -i "s/^#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen && locale-gen
+# configure locales
+sed -i "s/^#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen && locale-gen > /dev/null
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# creating hostname and hosts files
+# create hostname and hosts file
 echo "$2" > /etc/hostname
 tee /etc/hosts <<EOF > /dev/null
 127.0.0.1   localhost
@@ -17,7 +17,11 @@ tee /etc/hosts <<EOF > /dev/null
 127.0.1.1   $2.localdomain $2
 EOF
 
-echo "root:$3" | chpasswd
+# install grub
+grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/efi --bootloader-id=GRUB > /dev/null
+grub-mkconfig --output=/efi/grub/grub.cfg > /dev/null
 
-grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/efi --bootloader-id=GRUB
-grub-mkconfig --output=/efi/grub/grub.cfg
+# create a new user
+useradd -m -G wheel -s /bin/bash "$4"
+echo "$4:$5" | chpasswd
+echo "root:$3" | chpasswd
--- install.sh
+++ install.sh
@@ -2,19 +2,27 @@
 # Arch Linux installation script for UEFI systems
 
 # PARAMETERS
-hostname="eddies"
+
+# computer's info
+country="SG"        # country where we will be fetching our live mirrorlist from
 timezone="Asia/Jakarta"
-password="1"    # root password
-country="SG"    # country where we will be fetching our mirrorlist from
-root="5"        # partition size in GiB
-swap="1"        # partition size in GiB, the remainder will be assigned to /home
+hostname="eddies"
+
+# users login info
+username="anvity"
+userPassword="2"
+rootPassword="1"
+
+# / and swap partitions size in GiB, remainder will be assigned to /home partition
+rootSize="5"
+swapSize="1"
 
 mirrorlist() {
     echo "Fetching mirrorlist"
 
     pacman -Syy --noconfirm pacman-contrib > /dev/null 2>&1
 
-    # fetching and ranking a live mirrorlist
+    # fetch and ranks a live mirrorlist
     curl -s "https://www.archlinux.org/mirrorlist/?country=$country&protocol=https&ip_version=4" | \
     sed -e "s/^#Server/Server/g; /^#/d" | \
     rankmirrors -n 6 - > /etc/pacman.d/mirrorlist
@@ -23,14 +31,14 @@ mirrorlist() {
 partition() {
     echo "Partitioning"
 
-    # creating GPT partitions
-    root=$((root * 1024 + 261))
-    swap=$((swap * 1024 + root))
+    # create GPT partitions
+    rootSize=$((rootSize * 1024 + 261))
+    swapSize=$((swapSize * 1024 + root))
     parted -s /dev/sda mklabel gpt \
         mkpart efi fat32 1MiB 261MiB \
-        mkpart root ext4 261MiB "$root"MiB \
-        mkpart swap linux-swap "$root"MiB "$swap"MiB \
-        mkpart home ext4 "$swap"MiB 100% \
+        mkpart root ext4 261MiB "$rootSize"MiB \
+        mkpart swap linux-swap "$rootSize"MiB "$swapSize"MiB \
+        mkpart home ext4 "$swapSize"MiB 100% \
         set 1 esp on > /dev/null 2>&1
 
     # formatting
@@ -44,7 +52,7 @@ partition() {
     mount /dev/sda1 /mnt/efi > /dev/null 2>&1
     mount /dev/sda4 /mnt/home > /dev/null 2>&1
 
-    # initializing swap partition
+    # initialize swap partition
     mkswap /dev/sda3 > /dev/null 2>&1
     swapon /dev/sda3 > /dev/null 2>&1
 }
@@ -52,17 +60,19 @@ partition() {
 install() {
     echo "Installing packages"
 
-    pacstrap /mnt base base-devel grub efibootmgr > /dev/null 2>&1
+    pacstrap /mnt base base-devel \
+        networkmanager \
+        grub efibootmgr > /dev/null 2>&1
 
     genfstab -U /mnt >> /mnt/etc/fstab
 }
 
 configure() {
     echo "Configuring"
-    
+
     curl -s https://raw.githubusercontent.com/ifananvity/arch-installer/master/config.sh -o /mnt/config.sh
     chmod +x /mnt/config.sh
-    arch-chroot /mnt ./config.sh "$timezone" "$hostname" "$password"
+    arch-chroot /mnt ./config.sh "$timezone" "$hostname" "$rootPassword" "$username" "$userPassword"
     rm -f /mnt/config.sh
 }
 
