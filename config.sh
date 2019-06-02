3b564b9479514f60ef4af2c2996cf38a7e985c69
Tifan Dwi Avianto <ifan.anvity@gmail.com>
2019-06-02 14:32:03 +0700

    Reduce GRUB timeout to 1 sec

--- config.sh
+++ config.sh
@@ -17,11 +17,14 @@ tee /etc/hosts <<EOF > /dev/null
 127.0.1.1   $2.localdomain $2
 EOF
 
+# install grub and generate config file
 if [ -d /sys/firmware/efi/efivars ]; then
     grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/efi --bootloader-id=GRUB > /dev/null 2>&1
+    sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g" /etc/default/grub
     grub-mkconfig --output=/efi/grub/grub.cfg > /dev/null 2>&1
 else
     grub-install --target=i386-pc /dev/sda > /dev/null 2>&1
+    sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g" /etc/default/grub
     grub-mkconfig --output=/boot/grub/grub.cfg > /dev/null 2>&1
 fi
 
@@ -29,13 +32,14 @@ fi
 useradd -m -g users -G wheel -s /bin/bash "$4"
 echo "$4:$5" | chpasswd
 echo "root:$3" | chpasswd
-sed -i "s/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" /etc/sudoers
+sed -i "s/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: \/usr\/bin\/halt,\/usr\/bin\/poweroff,\/usr\/bin\/reboot,\/usr\/bin\/pacman -Syu,\/usr\/bin\/pacman -Syyu,\/usr\/bin\/pacman -Sy,\/usr\/bin\/pacman -Syy/g; /without a password/a Defaults \!tty_tickets" /etc/sudoers
 
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
+# import dotfiles
+curl https://raw.githubusercontent.com/ifananvity/dotfiles/master/.bashrc > /home/$4/.bashrc
+curl https://raw.githubusercontent.com/ifananvity/dotfiles/master/.xinitrc > /home/$4/.xinitrc
 
-curl -Os https://raw.githubusercontent.com/ifananvity/dotfiles/master/.bashrc \
-         https://raw.githubusercontent.com/ifananvity/dotfiles/master/.xinitrc
+# colorize output
+sed -i "s/^#Color/Color/g; /#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 
+# start these services at startup
 systemctl enable NetworkManager sshd > /dev/null 2>&1
-
-sed -i "s/^#Color/Color/; /#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
