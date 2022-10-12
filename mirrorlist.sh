462e2f07e8f2d0b5b304f933aebc11a4bd5d37b7
Tifan <tifan@proton.me>
2022-10-13 01:01:03 +0700

    update

--- mirrorlist.sh
+++ mirrorlist.sh
@@ -1,1 +1,6 @@
-test
+#!/bin/sh
+# Arch Linux installation script
+#
+
+curl -s "https://archlinux.org/mirrorlist/?country=ID&protocol=https&ip_version=4&use_mirror_status=on" | \
+sed -n "s/#Server/Server/pg; /Generated/p"
